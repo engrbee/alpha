001006ff1652b7bbfd9c9b800de851354152bb50
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-17 23:39:18 +0800

    Update baiducloud.js

--- js/baiducloud.js
+++ js/baiducloud.js
@@ -1,27 +1,6 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-if (body.indexOf("svip") != -1 && body.indexOf("emotional_tip_front") != -1) {
-  obj.svip = {};
-};
-
-if (body.indexOf("vip") != -1 && body.indexOf("emotional_tip_front") != -1) {
-  obj.vip = {};
-};
-
-if (body.indexOf("user_info") != -1 && body.indexOf("is_vip") != -1) {
-  obj.user_info.is_vip = 1;
-  obj.user_info.is_svip = 1;
-  obj.user_info.is_mvip = 1;
-};
-
-if (body.indexOf("tips_data_list") != -1 && body.indexOf("status_data") != -1) {
-  obj.tips_data_list = [{}];
-  obj.tips_data = {};
-  obj.status_data = "";
-  obj.status_data_arr = [""];
-};
-
 if (body.indexOf("product_infos") != -1) {
   obj.product_infos = [
     {
