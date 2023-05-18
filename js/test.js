0bb3871ac4c09c80eb39ea45155c0b8d6ac862ac
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-18 15:41:18 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -3,10 +3,11 @@ var res = $response;
 var url = $request.url;
 var method = $request.method;
 var body = $response.body;
+
 // var obj = JSON.parse(body);
 // var keys = Object.keys(obj);
+// console.log(keys);
+// console.log(body.slice(0,100));
 
 console.log(url);
 console.log(method);
-// console.log(keys);
-console.log(body.slice(0,100));
