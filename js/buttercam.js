fbe43511de35eafc54c691564baab03fe359a568
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-18 11:20:29 +0800

    Update buttercam.js

--- js/buttercam.js
+++ js/buttercam.js
@@ -2,7 +2,7 @@ var body = $response
   .body
   .replace(/\"ownership\":\"\w+\"/g, '"ownership":"free"')
   .replace(/\"usageType\":\"\w+\"/g, '"usageType":"unlimited"')
-  .replace(/\"memberships\":\[.*?\]/g, '"memberships":[{"endAt":4102415999,"id":"1","name":"Premium","ownership":"monthly","startAt":1587654321,"usageType":"unlimited"}]')
+  .replace(/\"memberships\":\[.*?\]/g, '"memberships":[{"endAt":4102415999,"id":"1","name":"普通会员","ownership":"temporary","startAt":1587654321,"usageType":"unlimited"}]')
   .replace(/false/g, "true");
 
 $done({body});
