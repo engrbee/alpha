59e4c676df010f2e15dfc12a4c96a1684223801d
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-21 17:08:42 +0800

    Update verify_receipt.js

--- js/verify_receipt.js
+++ js/verify_receipt.js
@@ -2,6 +2,9 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 if (body.indexOf("expires_date") != -1) {
+  obj["receipt"]["original_purchase_date"] = "2022-01-01 08:00:00 Etc/GMT";
+  obj["receipt"]["original_purchase_date_pst"] = "2022-01-01 08:00:00 America/Los_Angeles";
+  obj["receipt"]["original_purchase_date_ms"] = "1641024000000";
   obj["receipt"]["in_app"][0]["expires_date"] = "2099-12-31 23:59:59 Etc/GMT";
   obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-12-31 23:59:59 America/Los_Angeles";
   obj["receipt"]["in_app"][0]["expires_date_ms"] = "4102415999000";
