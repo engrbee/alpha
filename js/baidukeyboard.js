c4a63e4a02d688213514e14e4863a8b70c2b78e4
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-22 18:18:21 +0800

    Create baidukeyboard.js

---
+++ js/baidukeyboard.js
@@ -0,0 +1,9 @@
+var obj = {
+  "ecode": 0,
+  "emsg": "OK",
+  "data": {
+    "vip_info": "8MxeqfNsFYI_5xjP6O23I6pSAYyA5FjMIpAojIyZVf_TheoK85woYIMuBigia28bguvcfsvoA"
+  }
+};
+
+$done({body: JSON.stringify(obj)});
