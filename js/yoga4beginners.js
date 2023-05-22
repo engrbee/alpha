a2f9885381f56ff48fcd4886a254d3a0780aaaae
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-22 11:14:58 +0800

    Create yoga4beginners.js

---
+++ js/yoga4beginners.js
@@ -0,0 +1,139 @@
+var obj = {
+  "data": {
+    "type": "adapty_inapps_apple_receipt_validation_result",
+    "id": "fc7f62e0-360b-4f60-a45c-42a28c89f11a",
+    "attributes": {
+      "app_id": "9bd8f4ba-087e-4e08-a11f-18e6a8ee26f8",
+      "profile_id": "fc7f62e0-360b-4f60-a45c-42a28c89f11a",
+      "customer_user_id": null,
+      "total_revenue_usd": 0.0,
+      "paid_access_levels": {
+        "premium": {
+          "id": "premium",
+          "is_active": true,
+          "is_lifetime": true,
+          "expires_at": "2099-12-31T23:59:59.000000+0000",
+          "starts_at": null,
+          "will_renew": true,
+          "vendor_product_id": "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly",
+          "store": "app_store",
+          "activated_at": "2023-05-15T04:32:46.000000+0000",
+          "renewed_at": "2099-12-31T23:59:59.000000+0000",
+          "unsubscribed_at": null,
+          "billing_issue_detected_at": null,
+          "is_in_grace_period": false,
+          "active_introductory_offer_type": null,
+          "active_promotional_offer_type": null,
+          "active_promotional_offer_id": null,
+          "cancellation_reason": null,
+          "is_refund": false
+        }
+      },
+      "subscriptions": {
+        "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly": {
+          "is_active": true,
+          "is_lifetime": true,
+          "expires_at": "2099-12-31T23:59:59.000000+0000",
+          "starts_at": null,
+          "will_renew": true,
+          "vendor_product_id": "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly",
+          "vendor_transaction_id": "510001230288428",
+          "vendor_original_transaction_id": "510001230288428",
+          "store": "app_store",
+          "activated_at": "2023-05-15T04:32:46.000000+0000",
+          "renewed_at": "2099-12-31T23:59:59.000000+0000",
+          "unsubscribed_at": null,
+          "billing_issue_detected_at": null,
+          "is_in_grace_period": false,
+          "active_introductory_offer_type": null,
+          "active_promotional_offer_type": null,
+          "active_promotional_offer_id": null,
+          "cancellation_reason": null,
+          "is_sandbox": false,
+          "is_refund": false
+        }
+      },
+      "non_subscriptions": null,
+      "custom_attributes": {},
+      "apple_validation_result": {
+        "environment": "Production",
+        "receipt": {
+          "receipt_type": "Production",
+          "adam_id": 1382141225,
+          "app_item_id": 1382141225,
+          "bundle_id": "com.oneothergame.yoga.fitness.workouts.beginners",
+          "application_version": "4.21.0",
+          "download_id": 502416011468942430,
+          "version_external_identifier": 856599750,
+          "receipt_creation_date": "2023-05-22 02:56:36 Etc/GMT",
+          "receipt_creation_date_ms": "1684724196000",
+          "receipt_creation_date_pst": "2023-05-21 19:56:36 America/Los_Angeles",
+          "request_date": "2023-05-22 02:56:40 Etc/GMT",
+          "request_date_ms": "1684724200772",
+          "request_date_pst": "2023-05-21 19:56:40 America/Los_Angeles",
+          "original_purchase_date": "2023-05-08 13:47:31 Etc/GMT",
+          "original_purchase_date_ms": "1683553651000",
+          "original_purchase_date_pst": "2023-05-08 06:47:31 America/Los_Angeles",
+          "original_application_version": "4.21.0",
+          "in_app": [
+            {
+              "quantity": "1",
+              "product_id": "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly",
+              "transaction_id": "510001230288428",
+              "original_transaction_id": "510001230288428",
+              "purchase_date": "2023-05-15 04:32:45 Etc/GMT",
+              "purchase_date_ms": "1684125165000",
+              "purchase_date_pst": "2023-05-14 21:32:45 America/Los_Angeles",
+              "original_purchase_date": "2023-05-15 04:32:46 Etc/GMT",
+              "original_purchase_date_ms": "1684125166000",
+              "original_purchase_date_pst": "2023-05-14 21:32:46 America/Los_Angeles",
+              "expires_date": "2099-12-31 23:59:59 Etc/GMT",
+              "expires_date_ms": "4102415999000",
+              "expires_date_pst": "2099-12-31 23:59:59 America/Los_Angeles",
+              "web_order_line_item_id": "510000577880724",
+              "is_trial_period": "false",
+              "is_in_intro_offer_period": "false",
+              "in_app_ownership_type": "PURCHASED"
+            }
+          ]
+        },
+        "latest_receipt_info": [
+          {
+            "quantity": "1",
+            "product_id": "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly",
+            "transaction_id": "510001230288428",
+            "original_transaction_id": "510001230288428",
+            "purchase_date": "2023-05-15 04:32:45 Etc/GMT",
+            "purchase_date_ms": "1684125165000",
+            "purchase_date_pst": "2023-05-14 21:32:45 America/Los_Angeles",
+            "original_purchase_date": "2023-05-15 04:32:46 Etc/GMT",
+            "original_purchase_date_ms": "1684125166000",
+            "original_purchase_date_pst": "2023-05-14 21:32:46 America/Los_Angeles",
+            "expires_date": "2023-05-22 04:32:45 Etc/GMT",
+            "expires_date_ms": "4102415999000",
+            "expires_date_pst": "2023-05-22 04:32:45 America/Los_Angeles",
+            "web_order_line_item_id": "510000577880724",
+            "is_trial_period": "false",
+            "is_in_intro_offer_period": "false",
+            "in_app_ownership_type": "PURCHASED",
+            "subscription_group_identifier": "20663089"
+          }
+        ],
+        "latest_receipt": "MIIUzgYJKoZIhvcNAQcCoIIUvzCCFLsCAQExCzAJBgUrDgMCGgUAMIIEDAYJKoZIhvcNAQcBoIID/QSCA/kxggP1MAoCARQCAQEEAgwAMAsCARkCAQEEAwIBAzAMAgEKAgEBBAQWAjQrMAwCAQ4CAQEEBAICAL0wDQIBCwIBAQQFAgMX+pkwDQIBDQIBAQQFAgMCci0wDgIBAQIBAQQGAgRSYc0pMA4CAQkCAQEEBgIEUDI2MDAOAgEQAgEBBAYCBDMOrMYwEAIBAwIBAQQIDAY0LjIxLjAwEAIBEwIBAQQIDAY0LjIxLjAwEgIBDwIBAQQKAggG+PCzVRMsXjAUAgEAAgEBBAwMClByb2R1Y3Rpb24wGAIBBAIBAgQQoMsYplRViGVzQcEKD6ArNDAcAgEFAgEBBBTOtOPQoqOL7I5va7C5wveDKAuEYTAeAgEIAgEBBBYWFDIwMjMtMDUtMjJUMDI6NTY6MzZaMB4CAQwCAQEEFhYUMjAyMy0wNS0yMlQwMjo1Njo0MFowHgIBEgIBAQQWFhQyMDIzLTA1LTA4VDEzOjQ3OjMxWjAyAgEHAgEBBCoJiGj5B7EJxuWJ9NCFxU+Cy7cpjmviHZEU8ac6Az7/ZNbWyPx+R1iRMj4wOgIBAgIBAQQyDDBjb20ub25lb3RoZXJnYW1lLnlvZ2EuZml0bmVzcy53b3Jrb3V0cy5iZWdpbm5lcnMwWQIBBgIBAQRRVHIbYKcoBnCDGyKEifac8xsuvUz4t7T0uzQb+wrO5QkRzp0uBI64P+PJ0PNi/guZu570u6fe8tFzVp2JUfeFixjt41I5pH1R9TvGKpYNUGGKMIIBvQIBEQIBAQSCAbMxggGvMAsCAgatAgEBBAIMADALAgIGsAIBAQQCFgAwCwICBrICAQEEAgwAMAsCAgazAgEBBAIMADALAgIGtAIBAQQCDAAwCwICBrUCAQEEAgwAMAsCAga2AgEBBAIMADAMAgIGpQIBAQQDAgEBMAwCAgarAgEBBAMCAQMwDAICBrECAQEEAwIBATAMAgIGtwIBAQQDAgEAMAwCAga6AgEBBAMCAQAwDwICBq4CAQEEBgIEXCkcuDASAgIGrwIBAQQJAgcBz9fDR6KUMBoCAganAgEBBBEMDzUxMDAwMTIzMDI4ODQyODAaAgIGqQIBAQQRDA81MTAwMDEyMzAyODg0MjgwHwICBqgCAQEEFhYUMjAyMy0wNS0xNVQwNDozMjo0NVowHwICBqoCAQEEFhYUMjAyMy0wNS0xNVQwNDozMjo0NlowHwICBqwCAQEEFhYUMjAyMy0wNS0yMlQwNDozMjo0NVowTAICBqYCAQEEQwxBY29tLm9uZW90aGVyZ2FtZS55b2dhLmZpdG5lc3Mud29ya291dHMuYmVnaW5uZXJzLnN1cHBvcnRlcl95ZWFybHmggg7iMIIFxjCCBK6gAwIBAgIQLasDG73WZXPSByl5PESXxDANBgkqhkiG9w0BAQUFADB1MQswCQYDVQQGEwJVUzETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UECwwCRzcxRDBCBgNVBAMMO0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTIyMTIwMjIxNDYwNFoXDTIzMTExNzIwNDA1MlowgYkxNzA1BgNVBAMMLk1hYyBBcHAgU3RvcmUgYW5kIGlUdW5lcyBTdG9yZSBSZWNlaXB0IFNpZ25pbmcxLDAqBgNVBAsMI0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zMRMwEQYDVQQKDApBcHBsZSBJbmMuMQswCQYDVQQGEwJVUzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMDdxq606Lxt68F9tc6YWfZQWLZC3JXjGsX1z2Sqf9LMYUzWFON3gcRZMbcZx01Lq50nphw+VHJQIh49MB1KDkbl2CYpFUvjIJyu1fMlY9CY1HH4bpbzjqAKxQQ16Tj3q/g7lNoH5Vs5hf+deUD0GgqulVmY0xxcimwFfZofNEXBBM3VyZKlRhcGrKSF83dcH4X3o0Hm2xMQb23wIeqsJqZmPV6CFcdcmymWTX6KTo54u1fJNZR7tgDOGAqLdZWb6cMUPsEQNARttzw3M9/NFD5iDMDfL3K77Uq/48hpDX6WbR1PEDdu0/w9GgZ9bAEUyMRfMWpS8TMFyGDjxgPNJoECAwEAAaOCAjswggI3MAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUXUIQbBu7x1KXTkS9Eye5OhJ3gyswcAYIKwYBBQUHAQEEZDBiMC0GCCsGAQUFBzAChiFodHRwOi8vY2VydHMuYXBwbGUuY29tL3d3ZHJnNy5kZXIwMQYIKwYBBQUHMAGGJWh0dHA6Ly9vY3NwLmFwcGxlLmNvbS9vY3NwMDMtd3dkcmc3MDEwggEfBgNVHSAEggEWMIIBEjCCAQ4GCiqGSIb3Y2QFBgEwgf8wNwYIKwYBBQUHAgEWK2h0dHBzOi8vd3d3LmFwcGxlLmNvbS9jZXJ0aWZpY2F0ZWF1dGhvcml0eS8wgcMGCCsGAQUFBwICMIG2DIGzUmVsaWFuY2Ugb24gdGhpcyBjZXJ0aWZpY2F0ZSBieSBhbnkgcGFydHkgYXNzdW1lcyBhY2NlcHRhbmNlIG9mIHRoZSB0aGVuIGFwcGxpY2FibGUgc3RhbmRhcmQgdGVybXMgYW5kIGNvbmRpdGlvbnMgb2YgdXNlLCBjZXJ0aWZpY2F0ZSBwb2xpY3kgYW5kIGNlcnRpZmljYXRpb24gcHJhY3RpY2Ugc3RhdGVtZW50cy4wMAYDVR0fBCkwJzAloCOgIYYfaHR0cDovL2NybC5hcHBsZS5jb20vd3dkcmc3LmNybDAdBgNVHQ4EFgQUskV9w0SKa0xJr25R3hfJUUbv+zQwDgYDVR0PAQH/BAQDAgeAMBAGCiqGSIb3Y2QGCwEEAgUAMA0GCSqGSIb3DQEBBQUAA4IBAQB3igLdpLKQpayfh51+Xbe8aQSjGv9kcdPRyiahi3jzFSk+cMzrVXAkm1MiCbirMSyWePiKzhaLzyg+ErXhenS/QUxZDW+AVilGgY/sFZQPUPeZt5Z/hXOnmew+JqRU7Me+/34kf8bE5lAV8Vkb5PeEBysVlLOW6diehV1EdK5F0ajv+aXuHVYZWm3qKxuiETQNN0AU4Ovxo8d2lWYM281fG2J/5Spg9jldji0uocUBuUdd0cpbpVXpfqN7EPMDpIK/ybRVoYhYIgX6/XlrYWgQ/7jR7l7krMxyhGyzAhUrqjmvsAXmV1sPpCimKaRLh3edoxDfYth5aGDn+k7KyGTLMIIEVTCCAz2gAwIBAgIUNBhY/wH+Bj+O8Z8f6TwBtMFG/8kwDQYJKoZIhvcNAQEFBQAwYjELMAkGA1UEBhMCVVMxEzARBgNVBAoTCkFwcGxlIEluYy4xJjAkBgNVBAsTHUFwcGxlIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MRYwFAYDVQQDEw1BcHBsZSBSb290IENBMB4XDTIyMTExNzIwNDA1M1oXDTIzMTExNzIwNDA1MlowdTELMAkGA1UEBhMCVVMxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAsMAkc3MUQwQgYDVQQDDDtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKyu0dO2irEbKJWt3lFRTD8z4U5cr7P8AtJlTyrUdGiMdRdlzyjkSAmYcVIyLBZOeI6SVmSp3YvN4tTHO6ISRTcCGWJkL39hxtNZIr+r+RSj7baembov8bHcMEJPtrayxnSqYla77UQ2D9HlIHSTVzpdntwB/HhvaRY1w24Bwp5y1HE2sXYJer4NKpfxsF4LGxKtK6sH32Mt9YjpMhKiVVhDdjw9F4AfKduxqZ+rlgWdFdzd204P5xN8WisuAkH27npqtnNg95cZFIuVMziT2gAlNq5VWnyf+fRiBAd06R2nlVcjrCsk2mRPKHLplrAIPIgbFGND14mumMHyLY7jUSUCAwEAAaOB7zCB7DASBgNVHRMBAf8ECDAGAQH/AgEAMB8GA1UdIwQYMBaAFCvQaUeUdgn+9GuNLkCm90dNfwheMEQGCCsGAQUFBwEBBDgwNjA0BggrBgEFBQcwAYYoaHR0cDovL29jc3AuYXBwbGUuY29tL29jc3AwMy1hcHBsZXJvb3RjYTAuBgNVHR8EJzAlMCOgIaAfhh1odHRwOi8vY3JsLmFwcGxlLmNvbS9yb290LmNybDAdBgNVHQ4EFgQUXUIQbBu7x1KXTkS9Eye5OhJ3gyswDgYDVR0PAQH/BAQDAgEGMBAGCiqGSIb3Y2QGAgEEAgUAMA0GCSqGSIb3DQEBBQUAA4IBAQBSowgpE2W3tR/mNAPt9hh3vD3KJ7Vw7OxsM0v2mSWUB54hMwNq9X0KLivfCKmC3kp/4ecLSwW4J5hJ3cEMhteBZK6CnMRF8eqPHCIw46IlYUSJ/oV6VvByknwMRFQkt7WknybwMvlXnWp5bEDtDzQGBkL/2A4xZW3mLgHZBr/Fyg2uR9QFF4g86ZzkGWRtipStEdwB9uV4r63ocNcNXYE+RiosriShx9Lgfb8d9TZrxd6pCpqAsRFesmR+s8FXzMJsWZm39LDdMdpI1mqB7rKLUDUW5udccWJusPJR4qht+CrLaHPGpsQaQ0kBPqmpAIqGbIOI0lxwV3ra+HbMGdWwMIIEuzCCA6OgAwIBAgIBAjANBgkqhkiG9w0BAQUFADBiMQswCQYDVQQGEwJVUzETMBEGA1UEChMKQXBwbGUgSW5jLjEmMCQGA1UECxMdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxFjAUBgNVBAMTDUFwcGxlIFJvb3QgQ0EwHhcNMDYwNDI1MjE0MDM2WhcNMzUwMjA5MjE0MDM2WjBiMQswCQYDVQQGEwJVUzETMBEGA1UEChMKQXBwbGUgSW5jLjEmMCQGA1UECxMdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxFjAUBgNVBAMTDUFwcGxlIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDkkakJH5HbHkdQ6wXtXnmELes2oldMVeyLGYne+Uts9QerIjAC6Bg++FAJ039BqJj50cpmnCRrEdCju+QbKsMflZ56DKRHi1vUFjczy8QPTc4UadHJGXL1XQ7Vf1+b8iUDulWPTV0N8WQ1IxVLFVkds5T39pyez1C6wVhQZ48ItCD3y6wsIG9wtj8BMIy3Q88PnT3zK0koGsj+zrW5DtleHNbLPbU6rfQPDgCSC7EhFi501TwN22IWq6NxkkdTVcGvL0Gz+PvjcM3mo0xFfh9Ma1CWQYnEdGILEINBhzOKgbEwWOxaBDKMaLOPHd5lc/9nXmW8Sdh2nzMUZaF3lMktAgMBAAGjggF6MIIBdjAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUK9BpR5R2Cf70a40uQKb3R01/CF4wHwYDVR0jBBgwFoAUK9BpR5R2Cf70a40uQKb3R01/CF4wggERBgNVHSAEggEIMIIBBDCCAQAGCSqGSIb3Y2QFATCB8jAqBggrBgEFBQcCARYeaHR0cHM6Ly93d3cuYXBwbGUuY29tL2FwcGxlY2EvMIHDBggrBgEFBQcCAjCBthqBs1JlbGlhbmNlIG9uIHRoaXMgY2VydGlmaWNhdGUgYnkgYW55IHBhcnR5IGFzc3VtZXMgYWNjZXB0YW5jZSBvZiB0aGUgdGhlbiBhcHBsaWNhYmxlIHN0YW5kYXJkIHRlcm1zIGFuZCBjb25kaXRpb25zIG9mIHVzZSwgY2VydGlmaWNhdGUgcG9saWN5IGFuZCBjZXJ0aWZpY2F0aW9uIHByYWN0aWNlIHN0YXRlbWVudHMuMA0GCSqGSIb3DQEBBQUAA4IBAQBcNplMLXi37Yyb3PN3m/J20ncwT8EfhYOFG5k9RzfyqZtAjizUsZAS2L70c5vu0mQPy3lPNNiiPvl4/2vIB+x9OYOLUyDTOMSxv5pPCmv/K/xZpwUJfBdAVhEedNO3iyM7R6PVbyTi69G3cN8PReEnyvFteO3ntRcXqNx+IjXKJdXZD9Zr1KIkIxH3oayPc4FgxhtbCS+SsvhESPBgOJ4V9T0mZyCKM2r3DYLP3uujL/lTaltkwGMzd/c6ByxW69oPIQ7aunMZT7XZNn/Bh1XZp5m5MkL72NVxnn6hUrcbvZNCJBIqxw8dtk2cXmPIS4AXUKqK1drk/NAJBzewdXUhMYIBsTCCAa0CAQEwgYkwdTELMAkGA1UEBhMCVVMxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAsMAkc3MUQwQgYDVQQDDDtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eQIQLasDG73WZXPSByl5PESXxDAJBgUrDgMCGgUAMA0GCSqGSIb3DQEBAQUABIIBAJdyBMOnL3+809X2VrRQd41+fsGfCWnUboeed55hJosUc5IgtAlVK8NYG5Q87mhcfZOf3WfS/JIQ8mRMSCCaqnZpJcVnQv0L/tJVFQp4Tfxb7sVpxFWwefGJosNtnVYPn6guUa8RBXhjqOR8in6DAkZxXwuh1lPYxfqjumDGlnSRTt/XJ9m2aZXxYZ/8o4kN21hQyBJfpW0qlESxbAt6ClqoCnQNjwm1aypTCUjJKMtfrykjO8ixorZ5fnFaASkrB9vkRlutT7eLI82f014TXYNav9bfb+zBfmJpvU9xypiaTVYBsJxYG77vaJFxjoRF4uw3u+NoVuM9RyJ/RMADq2Q=",
+        "pending_renewal_info": [
+          {
+            "auto_renew_product_id": "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly",
+            "product_id": "com.oneothergame.yoga.fitness.workouts.beginners.supporter_yearly",
+            "original_transaction_id": "510001230288428",
+            "auto_renew_status": "1"
+          }
+        ],
+        "status": 0
+      },
+      "promotional_offer_eligibility": false,
+      "introductory_offer_eligibility": false
+    }
+  }
+};
+
+$done({body : JSON.stringify(obj)});
