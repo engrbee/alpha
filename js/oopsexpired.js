67bbfb0ea5ad3308a0c57e37c6851d9a32eebd31
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-22 09:58:31 +0800

    Update oopsexpired.js

--- js/oopsexpired.js
+++ js/oopsexpired.js
@@ -1,10 +1,9 @@
-var body = $response.body;
-var obj = JSON.parse(body);
+var obj = JSON.parse($response.body);
 
-obj.data.membership_type = 2;
+obj.data["membership_type"] = 2;
 obj.data.expires_date = null;
 obj.data.user_is_pro = true;
 obj.data.group_is_pro = true;
-obj.data.event = null;
+obj.data["event"] = null;
 
 $done({body: JSON.stringify(obj)});
