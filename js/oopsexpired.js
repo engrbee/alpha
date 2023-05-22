00f4deadc120c8c5ca3f72bac4cd4e0c45f2c00a
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-22 09:55:16 +0800

    Update oopsexpired.js

--- js/oopsexpired.js
+++ js/oopsexpired.js
@@ -1,35 +1,10 @@
-const path1 = "/functions/bindMembership";
-const path2 = "/users/";
-
-var url = $request.url;
 var body = $response.body;
 var obj = JSON.parse(body);
 
-if (url.indexOf(path1) != -1) {
-  obj.result.data = {
-    "status": 1,
-    "is_pro": true,
-    "membership_type": 2,
-    "membership_name": "yearly",
-    "expires_date": "2099-12-31T23:59:59.000Z"
-  };
-};
-
-if (url.indexOf(path2) != -1) {
-  obj.group = {
-    "__type": "Pointer",
-    "className": "UserGroup",
-    "objectId": "646a3a945e6bb82b8cdba051"
-  };
-  obj.membership = 2;
-  obj.pro = "yearly";
-  obj.isActive = true;
-  obj.expiredAt = {
-    "__type": "Date",
-    "iso": "2099-12-31T23:59:59.000Z"
-  };
-  obj.emailVerified = true;
-  obj.mobilePhoneVerified = true;
-};
+obj.data.membership_type = 2;
+obj.data.expires_date = null;
+obj.data.user_is_pro = true;
+obj.data.group_is_pro = true;
+obj.data.event = null;
 
 $done({body: JSON.stringify(obj)});
