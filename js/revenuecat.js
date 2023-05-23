66080e5833d33d73a9963cf7ceca36650267e6ec
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-23 09:28:47 +0800

    Update revenuecat.js

--- js/revenuecat.js
+++ js/revenuecat.js
@@ -6,6 +6,30 @@ if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
   resp.headers = $request.headers;
+} else if (ua['indexOf']('VSCO') != -1) {
+  obj.subscriber.entitlements = "membership" : {
+    "expires_date": "2099-12-31T23:59:59Z",
+    "grace_period_expires_date": null,
+    "product_identifier": "vscopro_global_5999_annual_7D_free",
+    "purchase_date": "2022-01-01T08:00:00Z"
+  };
+  obj.subscriber.subscriptions = {
+    "vscopro_global_5999_annual_7D_free": {
+      "auto_resume_date": null,
+      "billing_issues_detected_at": null,
+      "expires_date": "2099-12-31T23:59:59Z",
+      "grace_period_expires_date": null,
+      "is_sandbox": false,
+      "original_purchase_date": "2022-01-01T08:00:00Z",
+      "ownership_type": "PURCHASED",
+      "period_type": "active",
+      "purchase_date": "2022-01-01T08:00:00Z",
+      "refunded_at": null,
+      "store": "app_store",
+      "unsubscribe_detected_at": null
+    }
+  };
+  resp.body = JSON.stringify(obj);
 } else if (obj && obj.subscriber) {
   resp.body = JSON
     .stringify(obj)
