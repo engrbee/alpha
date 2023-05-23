b0e5635927dd21a54ce044820b7f8d167930ea53
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-23 16:45:21 +0800

    Update revenuecat.js

--- js/revenuecat.js
+++ js/revenuecat.js
@@ -9,11 +9,11 @@ if (typeof $response == "undefined") {
 } else if (obj && obj.subscriber) {
   resp.body = JSON
     .stringify(obj)
-    .replace(/\"expires_date\":\".*?"/g, '"expires_date":"2099-12-31T23:59:59Z"')
-    .replace(/\"purchase_date\":\".*?"/g, '"purchase_date":"2022-01-01T08:00:00Z"')
-    .replace(/\"first_seen\":\".*?"/g, '"first_seen":"2022-01-01T08:00:00Z"')
-    .replace(/\"original_purchase_date\":\".*?"/g, '"original_purchase_date":"2022-01-01T08:00:00Z"')
-    .replace(/\"unsubscribe_detected_at\":\".*?"/g, '"unsubscribe_detected_at":null')
+    .replace(/\"expires_date\":\".*?\"/g, '"expires_date":"2099-12-31T23:59:59Z"')
+    .replace(/\"purchase_date\":\".*?\"/g, '"purchase_date":"2022-01-01T08:00:00Z"')
+    .replace(/\"first_seen\":\".*?\"/g, '"first_seen":"2022-01-01T08:00:00Z"')
+    .replace(/\"original_purchase_date\":\".*?\"/g, '"original_purchase_date":"2022-01-01T08:00:00Z"')
+    .replace(/\"unsubscribe_detected_at\":\".*?\"/g, '"unsubscribe_detected_at":null')
     .replace(/\"period_type\":\"\w+\"/g, '"period_type":"active"');
 
   if (ua['indexOf']('VSCO') != -1) {
