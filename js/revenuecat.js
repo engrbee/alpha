8313380ad222db328d9089906ec000b70cccd8c0
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-23 10:09:13 +0800

    Update revenuecat.js

--- js/revenuecat.js
+++ js/revenuecat.js
@@ -15,36 +15,36 @@ if (typeof $response == "undefined") {
     .replace(/\"original_purchase_date\":\".*?"/g, '"original_purchase_date":"2022-01-01T08:00:00Z"')
     .replace(/\"unsubscribe_detected_at\":\".*?"/g, '"unsubscribe_detected_at":null')
     .replace(/\"period_type\":\"\w+\"/g, '"period_type":"active"');
-};
 
-if (ua['indexOf']('VSCO') != -1) {
-  obj.subscriber.non_subscriptions = {};
-  obj.subscriber.other_purchases = {};
-  obj.subscriber.entitlements = {
-    "membership": {
-      "expires_date": "2099-12-31T23:59:59Z",
-      "grace_period_expires_date": null,
-      "product_identifier": "vscopro_global_5999_annual_7D_free",
-      "purchase_date": "2022-01-01T08:00:00Z"
-    }
+  if (ua['indexOf']('VSCO') != -1) {
+    obj.subscriber.non_subscriptions = {};
+    obj.subscriber.other_purchases = {};
+    obj.subscriber.entitlements = {
+      "membership": {
+        "expires_date": "2099-12-31T23:59:59Z",
+        "grace_period_expires_date": null,
+        "product_identifier": "vscopro_global_5999_annual_7D_free",
+        "purchase_date": "2022-01-01T08:00:00Z"
+      }
+    };
+    obj.subscriber.subscriptions = {
+      "vscopro_global_5999_annual_7D_free": {
+        "auto_resume_date": null,
+        "billing_issues_detected_at": null,
+        "expires_date": "2099-12-31T23:59:59Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-01-01T08:00:00Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "active",
+        "purchase_date": "2022-01-01T08:00:00Z",
+        "refunded_at": null,
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      }
+    };
   };
-  obj.subscriber.subscriptions = {
-    "vscopro_global_5999_annual_7D_free": {
-      "auto_resume_date": null,
-      "billing_issues_detected_at": null,
-      "expires_date": "2099-12-31T23:59:59Z",
-      "grace_period_expires_date": null,
-      "is_sandbox": false,
-      "original_purchase_date": "2022-01-01T08:00:00Z",
-      "ownership_type": "PURCHASED",
-      "period_type": "active",
-      "purchase_date": "2022-01-01T08:00:00Z",
-      "refunded_at": null,
-      "store": "app_store",
-      "unsubscribe_detected_at": null
-    }
-  };
+  resp.body = JSON.stringify(obj);
 };
-resp.body = JSON.stringify(obj);
 
 $done(resp);
