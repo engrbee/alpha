633975d6bac9ff6a14dda06b92411ae7dea5cf27
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-23 16:17:20 +0800

    Update proxyman.js

--- js/proxyman.js
+++ js/proxyman.js
@@ -4,7 +4,7 @@ var obj = {
   "Code": 200,
   "Data": {
     "Build": "12900",
-    "LicenseKey": "PXM-2ED2-8F7A-9EB3-F7A8",
+    "LicenseKey": "PXM-2ED2-8F7A-9EB3-F7A8-712C",
     "Tittle": "Thank you for purchasing Proxyman! Your support helps us ship Proxyman even more awesome.",
     "sign": "UFhNLVFXRVItQVNERi1aWENWLVVJT1AtSEpLTA==",
     "deviceID": "D5FB34F4-2BE1-48B7-B8B6-0134995B2257",
