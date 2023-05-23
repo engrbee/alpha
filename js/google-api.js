1d009354bcb5d6f2e70ff82c98a94861d3c7c518
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-23 08:51:03 +0800

    Create google-api.js

---
+++ js/google-api.js
@@ -0,0 +1,26 @@
+// ^https:\/\/app-measurement\.com\/config\/app\/1?(.*?)*$ url reject
+// ^https:\/\/firebaseremoteconfig\.googleapis\.com\/v1\/projects\/.*\/?(.*?)*$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/sjyz.js
+
+var obj = {
+  'entries': {
+    'verify_receipt': 'false',
+    'reset_pro_if_transcation_id_missing': 'false',
+    'set_fake_pro_when_loading_offers_failed': 'false',
+    'verify_receipt_with_firebase': 'false',
+    'check_receipt_data': 'false',
+    'check_transcation_id_white_list': '',
+    'enable_dandanplay': 'true',
+    'verify_product_identifier': 'false',
+    'clean_etag_when_loading_offsers_failed': 'flase',
+    'blacklist': '{"list":[]}',
+    'detect_malformed': 'false',
+    'library_search_movie': '{"enable":true,"source":"tmdb"}',
+    'enable_search_lyrics': 'false'
+  },
+  'appName': 'me.shuifeng.Filebox',
+  'state': 'UPDATE',
+  'templateVersion': '42'
+};
+$done({
+  'body': JSON['stringify'](obj);
+});
