cef7679d1ca2ab118fe24b19f5cd37092f962388
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-24 16:02:55 +0800

    Update _template.js

--- js/_template.js
+++ js/_template.js
@@ -2,7 +2,10 @@ const path1 = "v1/subscriptions";
 const path2 = "v1/subscriptions/receipt";
 
 var url = $request.url;
-var obj = {};
+var body = $response.body;
+var obj = JSON.parse(body);
+
+var obj = JSON.parse($response.body);
 
 if (url.indexOf(path1) != -1) {
   obj = {}
@@ -14,10 +17,8 @@ if (url.indexOf(path2) != -1) {
 
 $done({body: JSON.stringify(obj)});
 
-
 var body = $response
   .body
   .replace(/currentPeriodEndDate":\d+/g, 'currentPeriodEndDate":4102415999000')
   .replace(/trialPeriodEndDate":\d+/g, 'trialPeriodEndDate":4102415999000');
-  
 $done({body: body});
