31c4e71f8b45c1d29fb0bba292dd74db60da4bf4
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-24 14:59:09 +0800

    Update xiangha.js

--- js/xiangha.js
+++ js/xiangha.js
@@ -1,36 +1,24 @@
+const path1 = "/user/permissions";
+const path2 = "/home/getUser";
+const path3 = "/vip/userinfo";
+const path4 = "/vip/myvip";
+
+var url = $request.url;
 var body = $response.body;
-var chxm1023 = JSON.parse(body);
+var obj = JSON.parse(body);
 
-const xiangha = '/user/info';
-const caipu = '/apiios_dq.xiangha.com';
-const info = 'xiangha.com';
-const home = '/home/getUser';
-
-if ($request.url.indexOf(caipu) != -1){
-  body = body.replace(/\"status":\w+/g, '\"status":true');
-  body = body.replace(/\"is_open_vip":\d+/g, '\"is_open_vip":1');
-  body = body.replace(/\"is_vip":\d+/g, '\"is_vip":2');
-  body = body.replace(/\"isCollection":\d+/g, '\"isCollection":1');
-  body = body.replace(/\"sex":"\d+"/g, '\"sex":"1"');
-  body = body.replace(/\"reg_date":".*?"/g, '\"reg_date":"2022-01-01"');
-  body = body.replace(/\"nickName":".*?"/g, '\"nickName":"Eric"');
-};
-
-if ($request.url.indexOf(info) != -1){
-  body = body.replace(/\"isShow":"\d+"/g, '\"isShow":"2"');
-  body = body.replace(/\"isVideo":"\d+"/g, '\"isVideo":"2"');
-  body = body.replace(/\"isVip":"\d+"/g, '\"isVip":"2"');
-};
-
-if ($request.url.indexOf(home) != -1){
-  chxm1023.data.nickName = "Eric";
-  chxm1023.data.vip = {
+if (url.indexOf(path1) != -1) {
+  obj.data.adBlock = "2";
+  $done({body: JSON.stringify(obj)});
+} else if (url.indexOf(path2) != -1) {
+  obj.data.nickName = "Eric";
+  obj.data.vip = {
       "isVip": 2,
       "level": "2",
       "vipState": "2",
       "maturity_time": "2099-12-31",
       "first_time": "2022-01-01",
-      "last_time": "2023-05-24",
+      "last_time": "2023-05-01",
       "text": "2099-12-31到期",
       "color": "#866100",
       "maturity_day": "2",
@@ -41,7 +29,30 @@ if ($request.url.indexOf(home) != -1){
       "subTitleColor": "",
       "xiangdou": 0
     };
-  body = JSON.stringify(chxm1023);
+  $done({body: JSON.stringify(obj)});
+} else if (url.indexOf(path3) != -1) {
+  obj.data.vip = {
+      "growth": "0",
+      "mark": "1",
+      "first_time": "1684894739",
+      "last_time": "1684894739",
+      "maturity_time": "2099-12-31",
+      "is_auto": "1",
+      "autoType": "1",
+      "type": "1",
+      "stall_id": "0",
+      "stall_title": "999天",
+      "pay_type": "1",
+      "is_del": "1",
+      "xh_code": "",
+      "is_vip": true
+    };
+  $done({body: JSON.stringify(obj)});
+} else if (url.indexOf(path4) != -1) {
+  var body = $response
+    .body
+    .replace(/\"vip":\\{.*?\\}/g, '"vip":{"growth":"0","mark":"1","first_time":"1684894739","last_time":"1684894739","maturity_time":"2099-12-31","is_auto":"1","autoType":"1","type":"1","stall_id":"0","stall_title":"999\u5929","pay_type":"1","is_del":"1","xh_code":"","is_vip":true}')
+    .replace(/\\d{4}\\-\\d{2}\\-\\d{2}/g, '2099-12-31')
+    .replace(/<i class=\\"vip-state.*?">/g, '<i class="vip-state is-vip ">');
+  $done({body: body});
 };
-
-$done({body});
