34e26f3594cdf3a87b509889ae9cc4c6fabfe2dd
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-25 16:29:00 +0800

    Update fantastical.js

--- js/fantastical.js
+++ js/fantastical.js
@@ -1,25 +1,47 @@
-const path1 = "v1/auth/device";
-const path2 = "v1/account/details";
+const path1 = "/auth/device";
+const path2 = "/account/details";
+const path3 = "/account/appstore-receipt";
 
-var url = $request.url;
-var obj = JSON.parse($response.body);
+let url = $request.url;
+let obj = JSON.parse($response.body);
 
 if (url.indexOf(path1) != -1) {
-  obj["subscription"] = {
+  obj.status = "success";
+  obj.subscription = {
     "autorenew": true,
     "expiration": "2099-12-31T23:59:59Z",
     "expires": "2099-12-31T23:59:59Z"
-  }
+  };
+  obj.scope = [
+    "notify",
+    "weather",
+    "keyvalue-watch",
+    "keyvalue-verification",
+    "schedjoules",
+    "scheduling",
+    "account",
+    "keyvalue",
+    "fantastical"
+  ];
 };
 
 if (url.indexOf(path2) != -1) {
-  obj["subscription"] = {
+  obj.subscription = {
     "autorenew": true,
-    "expiration": "2099-12-31T23:59:59Z",
+    "expiration": "2099-12-31T23:59:59.000000Z",
+    "uuid": "f1da7c78-e964-4367-915c-886edc794959",
     "subscription_type": "AppStore",
     "is_expired": false,
     "trial": false
-  }
+  };
+};
+
+if (url.indexOf(path3) != -1) {
+  obj.autorenew = true;
+  obj.expiration = "2099-12-31T23:59:59.000000Z";
+  obj.subscription_type = "AppStore";
+  obj.is_expired = false;
+  obj.trial = false;
 };
 
 $done({body: JSON.stringify(obj)});
