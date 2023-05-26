72209976ac8a649798c7f20d30c2182bcaa6fe72
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 23:51:49 +0800

    Create structured.js

---
+++ js/structured.js
@@ -0,0 +1,42 @@
+var obj = {
+  "request_date_ms": 1685024551888,
+  "request_date": "2023-05-25T14:22:31Z",
+  "subscriber": {
+    "non_subscriptions": {
+    },
+    "first_seen": "2022-01-01T08:00:00Z",
+    "original_application_version": "1682",
+    "other_purchases": {
+    },
+    "management_url": null,
+    "subscriptions": {
+      "today.structured.pro": {
+        "original_purchase_date": "2022-01-01T08:00:00Z",
+        "expires_date": "2099-12-31T23:59:59Z",
+        "is_sandbox": false,
+        "refunded_at": null,
+        "unsubscribe_detected_at": null,
+        "grace_period_expires_date": null,
+        "period_type": "active",
+        "purchase_date": "2022-01-01T08:00:00Z",
+        "billing_issues_detected_at": null,
+        "ownership_type": "PURCHASED",
+        "store": "app_store",
+        "auto_resume_date": null
+      }
+    },
+    "entitlements": {
+      "pro": {
+        "grace_period_expires_date": null,
+        "purchase_date": "2022-01-01T08:00:00Z",
+        "product_identifier": "today.structured.pro",
+        "expires_date": "2099-12-31T23:59:59Z"
+      }
+    },
+    "original_purchase_date": "2022-01-01T08:00:00Z",
+    "original_app_user_id": "BF6734C964654C63B28B94D82584C883",
+    "last_seen": "2023-05-25T09:04:30Z"
+  }
+};
+
+$done({body: JSON.stringify(obj)});
