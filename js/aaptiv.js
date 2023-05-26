5b80c2767c2bf3deb60d5e9db43953060a91aed3
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 15:42:25 +0800

    Update aaptiv.js

--- js/aaptiv.js
+++ js/aaptiv.js
@@ -6,8 +6,8 @@ var obj = {
   "first_name": "Eric",
   "last_name": "Huang",
   "name": "Eric Huang",
-  "secret": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlNDc3cifQ.eyJpc3MiOiJodHRwczovL2FhcHRpdi5jb20iLCJhdWQiOlsiaHR0cHM6Ly9hYXB0aXYuY29tIiwiaHR0cHM6Ly9hYXB0aXYuY29tIiwiaHR0cHM6Ly9hYXB0aXYuY29tIl0sImlhdCI6MTY4NTA4NjQ3MSwic3ViIjoxMTg4NTY1NSwidWlwIjp7InVzZXJfZW1haWwiOiJxa2RjdnJibnZ0QHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsInVzZXJfaWQiOjExODg1NjU1LCJ2aXNpdG9yX2lkIjoiNDYyMjY3MTYyMTk1Njc5OTkzIiwidXNlcl9vYmplY3RfaWQiOiI2NDcwMDJhY2ViNzE1YzAwMjc3ZTY4NjkiLCJ2aXNpdF9pZCI6IjQ2MjI2NzE2MjE5NTY3OTk5MyJ9LCJleHAiOjE2ODc2Nzg0NzF9.ddT7srbEH6-6GGYeH3YyWQWUpLxZWa4hZ6oTrjAP_UoLagHeonpFwkon_PITlCBs_DyHAd2maY-V2qr8qdPgsB4MfOrap3_gpsF0QQiCYq9Ui2nB4XQ2Q2MP98dcyyrAbA0-ppAgLZ5uHLFMNyY__oNfV6UD-Wqh2qWYOyiQj3ARIaFnIFodYodnwZom0POaGAi1S1Wa2ROEdeGqJIYTrKG-R-y0GJ0E0d308FOlsFVQwQl31IxNZcqwnsky597ReFo2b6LxuTKQOfB2NF9PULOFs3yXOoZYPG-T_PGm982bXiJmXIqvDDEWPtb1K9B67e2v8IwHB9n_kb9-vDSOHw",
-  "created": "2023-05-26T00:51:56.696Z",
+  "secret": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlNDc3cifQ.eyJpc3MiOiJodHRwczovL2FhcHRpdi5jb20iLCJhdWQiOlsiaHR0cHM6Ly9hYXB0aXYuY29tIiwiaHR0cHM6Ly9hYXB0aXYuY29tIiwiaHR0cHM6Ly9hYXB0aXYuY29tIl0sImlhdCI6MTY4NTA4NjQ3MCwic3ViIjoxMTg4NTY1NSwidWlwIjp7InVzZXJfZW1haWwiOiJxa2RjdnJibnZ0QHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsInVzZXJfaWQiOjExODg1NjU1LCJ2aXNpdG9yX2lkIjoiNDYyMjY3MTYyMTk1Njc5OTkzIiwidXNlcl9vYmplY3RfaWQiOiI2NDcwMDJhY2ViNzE1YzAwMjc3ZTY4NjkiLCJ2aXNpdF9pZCI6IjQ2MjI2NzE2MjE5NTY3OTk5MyJ9LCJleHAiOjE2ODc2Nzg0NzB9.bCu2ZM4cWdxp4VrwgBv2XLS3JUwK28gTymVVHLWEMqcqLkeilfR3NpniMcW_vTuaZZk_shyuUzaqbBlclJzZYS71IJgAnDNyzdjPUl101VGKquyi2WQJXKq8wZa5ZWHp6VDNeOgS6156PpDrOznLVnJENHvxeyzGz30fgmeMKRZqcwWmUwNjs7dT10PnEDCxmjvD3h01_p4okY2RgSx0mst5LNZ8tpAeKQWQ9eYfJloXMthBrfjM1xnMvTyDcQH6XjozThlvTuBkKS4jcAqPxN3JmQArW9CHfr5r3nqfvsDYjcTAWAw48g0nM8d9rJnuxYtuan-vrjsRwGJI2crtCw",
+  "created": "2022-01-01T08:00:00.000Z",
   "roles": [],
   "experiments": [],
   "config": {},
@@ -51,14 +51,63 @@ var obj = {
   "isActiveMember": true,
   "plan": "YEARLY",
   "source": "IOS",
-  "expirationDate": "12-31-2099",
+  "expirationDate": "12-31-2023",
   "membershipStatus": "ACTIVE",
-  "hasLegacyAccess": false,
+  "hasLegacyAccess": true,
   "currentIosAutoRenewPreference": {
     "auto_renew_status": "1"
   },
   "billingServiceData": {},
-  "heartRateZones": []
+  "heartRateZones": [
+    {
+      "min": 1,
+      "max": 89,
+      "labels": {
+        "zone": "Zone 0",
+        "range": "1 - 89 BPM"
+      }
+    },
+    {
+      "min": 90,
+      "max": 106,
+      "labels": {
+        "zone": "Zone 1",
+        "range": "90 - 106 BPM"
+      }
+    },
+    {
+      "min": 107,
+      "max": 124,
+      "labels": {
+        "zone": "Zone 2",
+        "range": "107 - 124 BPM"
+      }
+    },
+    {
+      "min": 125,
+      "max": 142,
+      "labels": {
+        "zone": "Zone 3",
+        "range": "125 - 142 BPM"
+      }
+    },
+    {
+      "min": 143,
+      "max": 160,
+      "labels": {
+        "zone": "Zone 4",
+        "range": "143 - 160 BPM"
+      }
+    },
+    {
+      "min": 161,
+      "max": 178,
+      "labels": {
+        "zone": "Zone 5",
+        "range": "161 - 178 BPM"
+      }
+    }
+  ]
 };
 
 $done({body: JSON.stringify(obj)});
