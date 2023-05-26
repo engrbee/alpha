bca0d7edc6ef63beb67bd4040e6490ac6885a0ef
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 12:32:13 +0800

    Create explaineverything.js

---
+++ js/explaineverything.js
@@ -0,0 +1,30 @@
+var obj = {
+  "id": 2761078,
+  "email": "salicat@163.com",
+  "accountType": "PA",
+  "accountDomain": "EE",
+  "avatar": "https://avatar.explaineverything.com/a/2023/05/26/01/2761078/ctPnCbNTWKqUdG.jpg?var=MED",
+  "isOrganizationAdmin": false,
+  "subscriptionEndDate": "2099-12-31T23:59:59Z",
+  "endOfGracePeriodDate": null,
+  "verificationSource": "APPSTORE",
+  "supervisor": {
+    "id": 2761078,
+    "displayName": "salicat",
+    "email": "salicat@163.com",
+    "accountType": "PA",
+    "verificationSource": "APPSTORE"
+  },
+  "consents": {
+    "marketing": false,
+    "terms": true
+  },
+  "roles": [
+    "User",
+    "Tenant"
+  ],
+  "screencasts": 0,
+  "name": "salicat"
+};
+
+$done({body: JSON.stringify(obj)});
