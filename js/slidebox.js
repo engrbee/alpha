d59475db234c10989b70e1e0576727edda324728
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 23:20:31 +0800

    Create slidebox.js

---
+++ js/slidebox.js
@@ -0,0 +1,17 @@
+var obj = {
+  "result" : {
+    "subscriptionTrialPeriod" : false,
+    "subscriptionExpirationDateMs" : 1872518379000,
+    "isEligibleForFreeTrial" : true,
+    "subscriptionProductId" : "com.Yooshr.Giftr.subscriptionPremium.Yearly",
+    "originalAppVersion" : "1205",
+    "subscriptionAutoRenewStatus" : 1,
+    "subscriber_platform" : "ios",
+    "isValidSubscriber" : true,
+    "isEligibleForSubscriptionOffer" : true,
+    "subscriptionDateLastVerified" : 1651654341272,
+    "subscriptionAutoRenewPreference" : "com.Yooshr.Giftr.subscriptionPremium.Yearly"
+  }
+};
+
+$done({body: JSON.stringify(obj)});
