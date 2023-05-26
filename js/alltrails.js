57cb11d923cf869681f91cec7821168a7895a989
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 12:14:37 +0800

    Create alltrails.js

---
+++ js/alltrails.js
@@ -0,0 +1,108 @@
+var obj = {
+  "users": [
+    {
+      "id": 62687692,
+      "private": null,
+      "username": "eric-huang-78",
+      "firstName": "Eric",
+      "lastName": "Huang",
+      "slug": "eric-huang-78",
+      "reputation": 0,
+      "reviews": 0,
+      "completed": 0,
+      "favorites": 0,
+      "following": 0,
+      "followers": 0,
+      "tracks": 0,
+      "maps": 0,
+      "photos": 0,
+      "lists": 0,
+      "activities": [],
+      "location": {
+        "postalCode": null,
+        "city": "Beijing",
+        "cityId": 44034,
+        "region": null,
+        "regionId": 1161,
+        "regionName": "Beijing",
+        "country": "CN",
+        "countryId": 145,
+        "countryName": "China",
+        "latitude": null,
+        "longitude": null
+      },
+      "calorieInfo": {
+        "sex": null,
+        "birthday": null,
+        "age": null,
+        "height": null,
+        "weight": null
+      },
+      "subscription": {
+        "provider": "apple",
+        "startDate": "2022-01-01T08:00:00Z",
+        "plan": "com.alltrails.AllTrails.pro.yearly.36.trial7.int",
+        "proStatus": "active",
+        "trial": false
+      },
+      "pro": true,
+      "admin": false,
+      "metric": true,
+      "displaySpeed": false,
+      "email": "gxz44dwrzn@privaterelay.appleid.com",
+      "email_none": true,
+      "profilePhoto": null,
+      "garminConnected": false,
+      "facebookConnected": false,
+      "googleConnected": false,
+      "appleConnected": true,
+      "marketingLangPreference": "en",
+      "locale": "en-US",
+      "isPromoEligible": false,
+      "trees_planted": 0,
+      "referralCode": "butsbf",
+      "referral_link": "https://www.alltrails.com/invite/BUTSBF",
+      "created": "2022-01-01T08:00:00Z",
+      "expirationDate": "2099-12-31T23:59:59Z",
+      "privacyPolicy": {
+        "version": "0",
+        "schemaVersion": "0",
+        "profile": {
+          "socialNetwork": {
+            "visibility": "urn:alltrails:visibility:public"
+          }
+        },
+        "content": {
+          "completedTrails": {
+            "visibility": "urn:alltrails:visibility:public"
+          },
+          "favoriteTrails": {
+            "visibility": "urn:alltrails:visibility:public"
+          },
+          "activities": {
+            "visibility": "urn:alltrails:visibility:public"
+          },
+          "customMaps": {
+            "visibility": "urn:alltrails:visibility:public"
+          },
+          "lists": {
+            "visibility": "urn:alltrails:visibility:public"
+          }
+        },
+        "socialNetwork": {
+          "followRequestsRequired": false
+        }
+      },
+      "phoneNumber": null,
+      "audiences": []
+    }
+  ],
+  "meta": {
+    "status": "ok",
+    "items": 1,
+    "timestamp": "2023-05-26T04:12:09Z"
+  },
+  "pageInfo": null
+};
+
+$done({body: JSON.stringify(obj)});
