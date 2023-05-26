36f2215d0eb7b3c46cbf1fc081d2bbe6350d4a3b
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 12:09:59 +0800

    Create agenda.js

---
+++ js/agenda.js
@@ -0,0 +1,15 @@
+var obj = {
+  "informUserOfPendingBonus": true,
+  "unlockExpiryMobile": null,
+  "universalPurchaseAvailable": true,
+  "freeTrialStatus": 0,
+  "hasLifetimeUnlock": true,
+  "error": 0,
+  "unlockExpiry": null,
+  "isSubscribed": true,
+  "isSubscribedMobile": true,
+  "licenseStatusMobile": 2,
+  "licenseStatus": 2
+};
+
+$done({body: JSON.stringify(obj)});
