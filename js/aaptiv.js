b78dc57297fd54d9e7118636256458699b0eb997
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-26 15:37:55 +0800

    Create aaptiv.js

---
+++ js/aaptiv.js
@@ -0,0 +1,64 @@
+var obj = {
+  "id": "647002aceb715c00277e6869",
+  "email": "eric@privaterelay.appleid.com",
+  "firstName": "Eric",
+  "lastName": "Huang",
+  "first_name": "Eric",
+  "last_name": "Huang",
+  "name": "Eric Huang",
+  "secret": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlNDc3cifQ.eyJpc3MiOiJodHRwczovL2FhcHRpdi5jb20iLCJhdWQiOlsiaHR0cHM6Ly9hYXB0aXYuY29tIiwiaHR0cHM6Ly9hYXB0aXYuY29tIiwiaHR0cHM6Ly9hYXB0aXYuY29tIl0sImlhdCI6MTY4NTA4NjQ3MSwic3ViIjoxMTg4NTY1NSwidWlwIjp7InVzZXJfZW1haWwiOiJxa2RjdnJibnZ0QHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsInVzZXJfaWQiOjExODg1NjU1LCJ2aXNpdG9yX2lkIjoiNDYyMjY3MTYyMTk1Njc5OTkzIiwidXNlcl9vYmplY3RfaWQiOiI2NDcwMDJhY2ViNzE1YzAwMjc3ZTY4NjkiLCJ2aXNpdF9pZCI6IjQ2MjI2NzE2MjE5NTY3OTk5MyJ9LCJleHAiOjE2ODc2Nzg0NzF9.ddT7srbEH6-6GGYeH3YyWQWUpLxZWa4hZ6oTrjAP_UoLagHeonpFwkon_PITlCBs_DyHAd2maY-V2qr8qdPgsB4MfOrap3_gpsF0QQiCYq9Ui2nB4XQ2Q2MP98dcyyrAbA0-ppAgLZ5uHLFMNyY__oNfV6UD-Wqh2qWYOyiQj3ARIaFnIFodYodnwZom0POaGAi1S1Wa2ROEdeGqJIYTrKG-R-y0GJ0E0d308FOlsFVQwQl31IxNZcqwnsky597ReFo2b6LxuTKQOfB2NF9PULOFs3yXOoZYPG-T_PGm982bXiJmXIqvDDEWPtb1K9B67e2v8IwHB9n_kb9-vDSOHw",
+  "created": "2023-05-26T00:51:56.696Z",
+  "roles": [],
+  "experiments": [],
+  "config": {},
+  "isStaff": false,
+  "isAdmin": false,
+  "permissions": [],
+  "onboardingQuestionnaireShown": true,
+  "onboardingQuestionnaireV3Shown": true,
+  "referral": {
+    "showTab": true,
+    "showNotification": false,
+    "showBadge": false
+  },
+  "connectedAccounts": {
+    "strava": {}
+  },
+  "userProfile": [
+    {
+      "avatar_image_url": null,
+      "birth_day": 2,
+      "birth_month": 12,
+      "birth_year": 1986,
+      "gender_description": null,
+      "gender_id": 1,
+      "height": 60,
+      "height_measurement_unit_id": 2,
+      "signup_source_id": null,
+      "user_id": 11885655,
+      "user_profile_id": 11319629,
+      "weight": 150,
+      "weight_measurement_unit_id": 1,
+      "id": 11319629,
+      "type": "user_profile",
+      "lactate_threshold": null
+    }
+  ],
+  "workoutsFinished": 0,
+  "isInWalledGarden": true,
+  "initialTabSelection": "browse",
+  "hasGoalConfig": false,
+  "isActiveMember": true,
+  "plan": "YEARLY",
+  "source": "IOS",
+  "expirationDate": "12-31-2099",
+  "membershipStatus": "ACTIVE",
+  "hasLegacyAccess": false,
+  "currentIosAutoRenewPreference": {
+    "auto_renew_status": "1"
+  },
+  "billingServiceData": {},
+  "heartRateZones": []
+};
+
+$done({body: JSON.stringify(obj)});
