bb516b08fb9d69e8b1548688b2c2e66db7d5d7f9
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-27 09:55:30 +0800

    Update filmicpro.js

--- js/filmicpro.js
+++ js/filmicpro.js
@@ -313,6 +313,11 @@
 
 var obj = JSON.parse($response.body);
 
-obj.me.active_bundle_subscriptions[0]["expiry"] = "2023-05-26T08:22:36+00:00";
+obj.me.active_bundle_subscriptions = [{
+    "features": [],
+    "expiry": "2099-12-31T23:59:59+00:00",
+    "product_id": "com.cinegenix.filmic.pro.1y_t70_1w"
+  }];
+obj.me.active_subscriptions_ids = ["com.cinegenix.filmic.pro.1y_t70_1w"];
 
 $done({body: JSON.stringify(obj)});
