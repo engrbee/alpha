028100a8c07be7c6d1a8e43104d8b3e36da9266d
engrbee <18159056+engrbee@users.noreply.github.com>
2023-05-28 23:25:46 +0800

    Update insighttimer.js

--- js/insighttimer.js
+++ js/insighttimer.js
@@ -11,8 +11,8 @@ if (url.indexOf(path1) != -1) {
 if (url.indexOf(path2) != -1) {
   obj.is_subscriber = true;
   obj.ios_free_trial_started = false;
-  obj.is_subscriber_removed_at = {"epoch": 4102415999000,"iso_8601_datetime_tz": "2099-12-31T23:59:59.000Z"};
-//  delete obj.is_subscriber_removed_at
+// obj.is_subscriber_removed_at = {"epoch": 4102415999000,"iso_8601_datetime_tz": "2099-12-31T23:59:59.000Z"};
+  delete obj.is_subscriber_removed_at
 };
 
 $done({body: JSON.stringify(obj)});
