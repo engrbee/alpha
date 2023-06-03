d2fe59c4821c23dd1b1dcc7e59c16af9a32bf729
engrbee <18159056+engrbee@users.noreply.github.com>
2023-06-03 18:00:27 +0800

    Create vox.js

---
+++ js/vox.js
@@ -0,0 +1,18 @@
+var obj = {
+  "id": "64662f7b17007303277304cd",
+  "accountType": 1,
+  "expirationDate": 4102415999,
+  "isAppleSubscription": true,
+  "subscriptionPeriod": "yearly",
+  "firstName": "Eric",
+  "lastName": "Huang",
+  "name": "",
+  "email": "eric@privaterelay.appleid.com",
+  "hasTracks": true,
+  "subscriptionStatus": "active",
+  "gracePeriod": 0,
+  "priceVersion": 2,
+  "gauid": ""
+};
+
+$done({body: JSON.stringify(obj)});
