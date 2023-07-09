c3be5d37456a70c4e5a7e6b64b81c26fc896b7c6
engrbee <18159056+engrbee@users.noreply.github.com>
2023-07-09 21:49:11 +0800

    Update timetree.js

--- js/timetree.js
+++ js/timetree.js
@@ -14,23 +14,26 @@ if (url.indexOf(path1) != -1) {
 };
 
 if (url.indexOf(path2) != -1) {
-  let obj = {
-    "user_subscription": {
-      "user_id": 15424269,
-      "uuid": "cc995f7b-133c-423c-abc8-fece2c739660",
-      "trial_used": false,
-      "current_subscription": {
-        "subscription_token": "510001226397691",
-        "plan": 0,
-        "status": 1,
-        "platform": 1,
-        "platform_status": "ACTIVE",
-        "in_trial": false,
-        "auto_renewal": true,
-        "cancelled": false,
-        "expire_at": 4102415999000
+  if body {
+    let obj = {
+      "user_subscription": {
+        "user_id": 15424269,
+        "uuid": "cc995f7b-133c-423c-abc8-fece2c739660",
+        "trial_used": false,
+        "current_subscription": {
+          "subscription_token": "510001226397691",
+          "plan": 0,
+          "status": 1,
+          "platform": 1,
+          "platform_status": "ACTIVE",
+          "in_trial": false,
+          "auto_renewal": true,
+          "cancelled": false,
+          "expire_at": 4102415999000
+        }
       }
-    }
-  };
+    };
   $done({status: 200, body: JSON.stringify(obj)});
+} else {
+  $done();
 };
