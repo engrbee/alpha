ec8157d3e19449ca58346dbdedc22951798b822a
engrbee <18159056+engrbee@users.noreply.github.com>
2023-07-09 21:24:02 +0800

    Update timetree.js

--- js/timetree.js
+++ js/timetree.js
@@ -1,8 +1,36 @@
+const path1 = "batch";
+const path2 = "user/subscription";
+
+let url = $request.url;
 let body = $response.body;
 
-if (body.indexOf("user_subscription") != -1) {
-  let new_body = body.replace(/"user_subscription".*?\}/g, '"user_subscription":{"user_id":15424269,"uuid":"cc995f7b-133c-423c-abc8-fece2c739660","trial_used":false,"current_subscription":{"subscription_token":"510001226397691","plan":0,"status":1,"platform":1,"platform_status":"ACTIVE","in_trial":false,"auto_renewal":true,"cancelled":false,"expire_at":4102415999000}}');
-  $done({body: new_body});
-} else {
-  $done({body: body});
+if (url.indexOf(path1) != -1) {
+  if (body.indexOf("user_subscription") != -1) {
+    let new_body = body.replace(/"user_subscription".*?\}/g, '"user_subscription":{"user_id":15424269,"uuid":"cc995f7b-133c-423c-abc8-fece2c739660","trial_used":false,"current_subscription":{"subscription_token":"510001226397691","plan":0,"status":1,"platform":1,"platform_status":"ACTIVE","in_trial":false,"auto_renewal":true,"cancelled":false,"expire_at":4102415999000 }}');
+    $done({body: new_body});
+  } else {
+    $done({body: body});
+  };
+};
+
+if (url.indexOf(path2) != -1) {
+  let obj = {
+    "user_subscription": {
+      "user_id": 15424269,
+      "uuid": "cc995f7b-133c-423c-abc8-fece2c739660",
+      "trial_used": false,
+      "current_subscription": {
+        "subscription_token": "510001226397691",
+        "plan": 0,
+        "status": 1,
+        "platform": 1,
+        "platform_status": "ACTIVE",
+        "in_trial": false,
+        "auto_renewal": true,
+        "cancelled": false,
+        "expire_at": 4102415999000
+      }
+    }
+  };
+  $done({body: JSON.stringify(obj)});
 };
