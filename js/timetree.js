7b5a168a6b036152fd7d03c6a7108345f257e63b
engrbee <18159056+engrbee@users.noreply.github.com>
2023-07-10 08:28:45 +0800

    Update timetree.js

--- js/timetree.js
+++ js/timetree.js
@@ -17,5 +17,5 @@ if (url.indexOf(path1) != -1) {
 if (url.indexOf(path2) != -1) {
   body = '"user_subscription":{"user_id":15424269,"uuid":"cc995f7b-133c-423c-abc8-fece2c739660","trial_used":false,"current_subscription":{"subscription_token":"510001226397691","plan":1,"status":1,"platform":1,"platform_status":"ACTIVE","in_trial":false,"auto_renewal":true,"cancelled":false,"expire_at":4102415999000 }}'
   headers["Content-Type"] = "application/json";
-  $done({response:{status:200, body:resp_body, headers:headers}});
+  $done({response:{status:200, body:body, headers:headers}});
 };
