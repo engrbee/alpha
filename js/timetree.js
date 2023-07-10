ff865a60d0749be5829ed828dfe69ab4b056a11f
engrbee <18159056+engrbee@users.noreply.github.com>
2023-07-10 08:15:06 +0800

    Update timetree.js

--- js/timetree.js
+++ js/timetree.js
@@ -6,7 +6,7 @@ let body = $response.body || "";
 
 if (url.indexOf(path1) != -1) {
   if (body.indexOf("user_subscription") != -1) {
-    let new_body = body.replace(/"user_subscription".*?\}/g, '"user_subscription":{"user_id":15424269,"uuid":"cc995f7b-133c-423c-abc8-fece2c739660","trial_used":false,"current_subscription":{"subscription_token":"510001226397691","plan":0,"status":1,"platform":1,"platform_status":"ACTIVE","in_trial":false,"auto_renewal":true,"cancelled":false,"expire_at":4102415999000 }}');
+    let new_body = body.replace(/"user_subscription".*?\}/g, '"user_subscription":{"user_id":15424269,"uuid":"cc995f7b-133c-423c-abc8-fece2c739660","trial_used":false,"current_subscription":{"subscription_token":"510001226397691","plan":1,"status":1,"platform":1,"platform_status":"ACTIVE","in_trial":false,"auto_renewal":true,"cancelled":false,"expire_at":4102415999000 }}');
     $done({body: new_body});
   } else {
     $done({body: body});
@@ -14,26 +14,6 @@ if (url.indexOf(path1) != -1) {
 };
 
 if (url.indexOf(path2) != -1) {
-  if (body) {
-    let obj = {
-      "user_subscription": {
-        "user_id": 15424269,
-        "uuid": "cc995f7b-133c-423c-abc8-fece2c739660",
-        "trial_used": false,
-        "current_subscription": {
-          "subscription_token": "510001226397691",
-          "plan": 0,
-          "status": 1,
-          "platform": 1,
-          "platform_status": "ACTIVE",
-          "in_trial": false,
-          "auto_renewal": true,
-          "cancelled": false,
-          "expire_at": 4102415999000
-        }
-      }
-    };
-  $done({status: 200, body: JSON.stringify(obj)});
-} else {
+  body = '"user_subscription":{"user_id":15424269,"uuid":"cc995f7b-133c-423c-abc8-fece2c739660","trial_used":false,"current_subscription":{"subscription_token":"510001226397691","plan":1,"status":1,"platform":1,"platform_status":"ACTIVE","in_trial":false,"auto_renewal":true,"cancelled":false,"expire_at":4102415999000 }}'
   $done({status: 200, body: body});
 };
