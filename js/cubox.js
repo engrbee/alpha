2949a631ef9e69a847f929b76ddd7c3d8ca2a995
engrbee <18159056+engrbee@users.noreply.github.com>
2023-07-28 11:27:55 +0800

    Update cubox.js

--- js/cubox.js
+++ js/cubox.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body);
 
-obj.data.expireTime = "2029-12-31T23:59:59Z";
+obj.data.expireTime = "2099-12-31T23:59:59Z";
 obj.data.isExpire = false;
 
 $done({body: JSON.stringify(obj)});
