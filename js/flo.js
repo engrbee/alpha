3454aa3df9c5fe9cbdb675347965d345e0eea3a2
engrbee <18159056+engrbee@users.noreply.github.com>
2024-12-08 19:56:51 +0800

    Update flo.js

--- js/flo.js
+++ js/flo.js
@@ -6,11 +6,11 @@ obj = {
   "auto_renew_status": true,
   "transaction_id": 510001215159193,
   "features": [],
-  "expiration_date": "2099-12-31T23:59:59Z",
+  "expiration_date": "2029-12-31T23:59:59Z",
   "subscription_manager": {
     "type": "apple",
     "url": "https://apps.apple.com/account/subscriptions"
   }
 };
 
-$done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(obj), status: 200});
